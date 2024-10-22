1b835d1a4384a3e7443c1fbf47fe98dbcef78529
Hasnat79 <hasnatabdullah79@gmail.com>
2024-10-22 12:25:26 -0500

    data imports as dataframes

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+en_super_set_path = "/home/grads/h/hasnat.md.abdullah/h2i_hatespeech_to_image/data/en_hf.csv"
+# ucb_hatespeech_path =""
+
+en_hatespeech_super_set = pd.read_csv(en_super_set_path)
+# ucb_hatespeech = pd.read_csv(ucb_hatespeech_path)
