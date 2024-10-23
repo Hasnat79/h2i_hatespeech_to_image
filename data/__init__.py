d90556a65d8619e16766345fcec19f94676d06d2
hasnat79 <hasnatabdullah79@gmail.com>
2024-10-23 18:28:18 -0500

    image generation pipeline created with dummy_images

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-en_super_set_path = "/home/grads/h/hasnat.md.abdullah/h2i_hatespeech_to_image/data/en_hf.csv"
+en_super_set_path = "/scratch/user/hasnat.md.abdullah/h2i_hatespeech_to_image/data/en_hf.csv"
 # ucb_hatespeech_path =""
 
 en_hatespeech_super_set = pd.read_csv(en_super_set_path)
 # ucb_hatespeech = pd.read_csv(ucb_hatespeech_path)
+
+
+super_set_neg_gender = pd.read_csv("/scratch/user/hasnat.md.abdullah/h2i_hatespeech_to_image/data/processed_data/en_hatespeech_superset/negative/gender_negative.csv")
+super_set_neg_race = pd.read_csv("/scratch/user/hasnat.md.abdullah/h2i_hatespeech_to_image/data/processed_data/en_hatespeech_superset/negative/race_negative.csv")
+super_set_neg_disability = pd.read_csv("/scratch/user/hasnat.md.abdullah/h2i_hatespeech_to_image/data/processed_data/en_hatespeech_superset/negative/disability_negative.csv")
+super_set_neg_lgbt = pd.read_csv("/scratch/user/hasnat.md.abdullah/h2i_hatespeech_to_image/data/processed_data/en_hatespeech_superset/negative/lgbt_negative.csv")
+
+
--- src/sd_large.py
+++ src/sd_large.py
@@ -1,26 +1,70 @@
 
 # Gen
 import os
-# Image Generation
+import sys
+sys.path.insert(0,'h2i_hatespeech_to_image/data')
 from diffusers import StableDiffusion3Pipeline
 import torch
-# Image similarity
 import warnings
 warnings.filterwarnings('ignore')
 from huggingface_hub import login
+from PIL import Image
 
-DEVICE = 'cuda' if torch.cuda.is_available else 'cpu' # device
-NUM_INFERENCE_STEPS = 2 # Number of inference steps to the Diffusion Model
-login(HUGGINGFACE_TOKEN)
-model = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.float16)
-# model = torch.nn.DataParallel(model)
-model = model.to(DEVICE)
+from data import super_set_neg_gender,super_set_neg_race,super_set_neg_disability,super_set_neg_lgbt
+# image generation parameters
+NUMBER_INFERENCE_STEPS = 28
+GUIDANCE_SCALE = 3.5
+# def generate_image_sd_large(model, prompt, number_inference_steps, guidance_scale,output_path):
+#     image = model(
+#     "A capybara holding a sign that reads Hello World",
+#     num_inference_steps=28,
+#     guidance_scale=3.5,
+# ).images[0]
+# image.save(output_path)
+def dummy_image():
+    return Image.new('RGB', (100, 100), color = 'red')
+if __name__ == "__main__":
+    # login("HF_TOKEN")
+    # Load the model
+    # model = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16, cache_dir = "cache_dir/")
+    # model = model.to("cuda")
+    
+    output_dir = "h2i_hatespeech_to_image/data/generated_images/negative"
+    filter_keywords = ['gender','race','disability','lgbt']
+    data_frames = [super_set_neg_gender,super_set_neg_race,super_set_neg_disability, super_set_neg_lgbt]
 
-prompt ="A capybara holding a sign that reads Hello World"
-output_image = model(prompt,
-                     negative_prompts="",
-                     num_inference_steps=NUM_INFERENCE_STEPS,
-                     height= 32,
-                     width= 32,
-                     guidance_scale = 7.0).images[0]
-output_image.save("output.png")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    # The following section is for testing purposes, remove when running the actual image generation
+    #------------------------------------------------
+    i = 0
+    #------------------------------------------------
+    for filter_keyword, df in zip(filter_keywords, data_frames):
+        print(f"Generating images for {filter_keyword}")
+        filter_dir = os.path.join(output_dir, filter_keyword)
+        if not os.path.exists(filter_dir):
+            os.makedirs(filter_dir)
+
+        for index, row in df.iterrows():
+            print(f"Generating image for index: {index}")
+            prompt = row['text']  # Assuming the dataframe has a column named 'text'
+            # image = generate_image_sd_large(model, prompt, NUMBER_INFERENCE_STEPS, GUIDANCE_SCALE)
+            try: 
+                image = dummy_image()
+                image_path = os.path.join(filter_dir, f"image_{index}_sdl.png")
+                image.save(image_path)
+                print(f"Image saved at {image_path}")
+                # The following section is for testing purposes, remove when running the actual image generation
+                #------------------------------------------------
+                i+=1
+                if i == 5:
+                    i=0
+                    break
+                #------------------------------------------------
+            except Exception as e:
+                print(f"Error generating image for {index}")
+                print(e)
+                continue
+
+
+
