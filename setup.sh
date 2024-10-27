04cbee40039ffeca45e42b8c0e7a0f4b9edf0b9d
hasnat79 <hasnatabdullah79@gmail.com>
2024-10-27 10:33:47 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 conda remove -n "h2i" --all
 conda create -n "h2i" python=3.9
 conda activate h2i
+pip install diffusers==0.31.0 transformers==4.46.0 accelerate==1.0.1
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-pip install transformers==4.44.2
-pip install diffusers==0.30.3
+pip install protobuf
+pip install sentencepiece
 
 
